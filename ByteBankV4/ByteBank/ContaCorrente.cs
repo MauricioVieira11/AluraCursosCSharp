16cf6bc3eac8a82a344dee1d5ea4ded84ffbe1e8
Mauricio <mauriciovieira8@hotmail.com>
2021-11-02 15:38:16 -0300

    Exceçoes com try catch. ParamName no ArgumentException

---
+++ ByteBankV4/ByteBank/ContaCorrente.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+
+namespace ByteBank
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+       
+        public int Numero { get; }
+            
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTRansferenciasNãoPermitidas { get; private set; }
+
+        public int _agencia;
+        public int Agencia { get; }
+        
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (numero <= 0 )
+            {
+                throw new ArgumentException("O argumento agência deve ser maior que 0",nameof(agencia));
+    
+            }
+            if(numero <= 0)
+            {
+                throw new ArgumentException("O argumento deve ser maior que 0.",nameof(numero));
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para o saque.", nameof (valor));
+            }
+
+
+            if (_saldo < valor)
+            {
+                ContadorSaquesNaoPermitidos++;
+
+                throw new SaldoInsuficienteException(Saldo, valor);    
+            }
+
+            _saldo -= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
+            }
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+
+                ContadorTRansferenciasNãoPermitidas++;
+                throw new SaldoInsuficienteException("Operação não realizada.");
+                
+            }
+
+
+            Sacar(valor);
+            contaDestino.Depositar(valor);
+            
+        }
+    }
+}
---
+++ ByteBankV4/ByteBank/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(514, 4578420);
+                ContaCorrente conta2 = new ContaCorrente(485, 456478);
+
+                conta2.Transferir(-10, conta);
+
+                conta.Depositar(50);
+                Console.WriteLine(conta.Saldo);
+                conta.Sacar(500);
+
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("|Argumento com problema: "+ ex.ParamName);
+                Console.WriteLine("Ocorreu uma exceção do tipo ArgumentException");
+                Console.WriteLine(ex.Message);
+            }
+            catch(SaldoInsuficienteException ex)
+            {
+                Console.WriteLine(ex.Saldo);
+                Console.WriteLine(ex.ValorSaque);
+
+                Console.WriteLine(ex.StackTrace);
+
+                
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Exceção do tipo saldo insuficente exception");
+            }
+            catch(Exception ex)
+            {
+                
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Metodo();
+            }
+            
+            
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Não é possível dividir por zero.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Aconteceu um erro!");
+            }
+           
+
+            Console.ReadLine();
+        }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                Dividir(10, divisor);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Fui capturado pelo (NullReferenceException ex)");
+                Console.WriteLine(ex.StackTrace);
+            }
+           
+            
+                int resultado = Dividir(10, divisor);
+                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+            
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com um número+ "+ numero+" e divisor"+divisor);
+                throw;
+                Console.WriteLine("Código depois do throw");
+
+
+            }
+
+            //return numero / divisor;
+
+
+        }
+    }
+}
